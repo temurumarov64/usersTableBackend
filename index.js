2fc84fd8b6a60ba49d530b5b27facbf5ddf5370d
Temur Umarov <umarovtemur25@gmail.com>
2023-06-04 18:32:47 +0500

    vercel added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 4200;
+const port = process.env.PORT || 4200;
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
